c194b544289d77b812624e1bbf28541ef743b9e4
edu1694 <eduardocardenas.16941694@gmail.com>
2022-05-11 00:35:14 -0400

    commit

--- js/index_query.js
+++ js/index_query.js
@@ -2,27 +2,3 @@ $('#btn-index').click(function() {
     window.open('https://www.mercadolibre.cl/alimento-leonardo-kitten-para-gato-de-temprana-edad-sabor-mix-en-bolsa-de-75kg/p/MLC17496696?pdp_filters=item_id:MLC946834578#searchVariation=MLC17496696&position=5&search_layout=stack&type=pad&tracking_id=13e8267d-ee1a-4a03-a161-e98636a82c1f');
     return false;
 });
-
-$('#btn-contac').click(function resetform() {
-    let nombre = $('#fname').val();
-    let apellido = $('#lname').val();
-    let message = $('#message').val();
-    if (nombre.length < 3) {
-        alert('El nombre debe tener minimo 3 caracterres y es obligatorio');
-        return false;
-    }
-    if (apellido.length < 3) {
-        alert('El apellido debe tener minimo 3 caracterres y es obligatorio');
-        return false;
-    }
-    if ($("#email").val().indexOf('@', 0) == -1 || $("#email").val().indexOf('.', 0) == -1) {
-        alert('El correo electrónico introducido no es correcto.');
-        return false;
-    }
-    if (message.length < 10) {
-        alert('La descripcion debe tener minimo 10 caracterres y es obligatorio');
-        return false;
-    }
-    alert('Sus datos se han guardado conrrectamente');
-    $("form input[type=text] , form textarea").each(function() { this.value = '' });
-});
---
+++ js/validador.js
@@ -0,0 +1,53 @@
+$.validator.setDefaults({
+    submitHandler: function() {
+        $("form input[type=text] , form textarea, form select").each(function() { this.value = '' });
+    }
+});
+
+
+$(document).ready(function() {
+    $('#formulario_mascota').validate({
+        rules: {
+            fullname: {
+                required: true,
+                minlength: 5
+            },
+            comments: {
+                required: true,
+                minlength: 10
+            },
+            email: {
+                required: true,
+                email: true,
+            },
+
+        },
+        messages: {
+            fullname: {
+                required: "Por favor ingresa tu nombre completo",
+                minlength: "Tu nombre debe ser de no menos de 5 caracteres"
+            },
+            comments: {
+                required: "Por favor ingresa un comentario",
+                minlength: "Tu nombre debe ser de no menos de 10 caracteres"
+            },
+            email: "Por favor ingresa un correo válido",
+        },
+        errorElement: "em",
+        errorPlacement: function(error, element) {
+            error.addClass("help-block");
+
+            if (element.prop("type") === "checkbox") {
+                error.insertAfter(element.parent("label"));
+            } else {
+                error.insertAfter(element);
+            }
+        },
+        highlight: function(element, errorClass, validClass) {
+            $(element).parents(".col-sm-10").addClass("has-error").removeClass("has-success");
+        },
+        unhighlight: function(element, errorClass, validClass) {
+            $(element).parents(".col-sm-10").addClass("has-success").removeClass("has-error");
+        }
+    });
+});
